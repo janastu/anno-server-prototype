27e4d475604afc819c01ce9f99cf52fb38530015
salus-sage <salus.sage@gmail.com>
2016-05-21 22:54:46 +0530

    bug fix: refactore+add requirements.txt

--- anno_api_hackday.py
+++ anno_api_hackday.py
@@ -14,16 +14,22 @@ app = Flask(__name__)
 
 # Global configuration variables
 # Access to variables as config['key'] and config['passphrase']
+# api_url is the cloudant database url
+# Register and create db @ cloudant.com
 config = {
     "key":"peredgerdedediffeetheret",
-    "passphrase":"292ce457c87db9ec202e9bd60ec833426dba271e"
+    "passphrase":"292ce457c87db9ec202e9bd60ec833426dba271e",
+    "api_url": "http://jants.janastu.org/annos/",
+    "DEBUG": True,
+    "HOST": '0.0.0.0'
    }	
 
 
 # Cloudant database endpoint
-anno_api = "http://jants.janastu.org/annos/"
+# anno_api = "http://jants.janastu.org/annos/"
 
 
+# what is this for?
 schema = {
     "type" : "object",
     "properties" : {
@@ -36,7 +42,7 @@ schema = {
 # Get list of annotations
 @app.route('/annotations/api/v1.0/annos', methods=['GET'])
 def get_annotations():
-    api_req =  anno_api+"_all_docs"
+    api_req =  config['api_url']+"_all_docs"
     response = requests.get(api_req)
     response = response.json()
     return jsonify({'annotations': response['rows']})
@@ -45,7 +51,7 @@ def get_annotations():
 # Get annotation with id = anno_id
 @app.route('/annotations/api/v1.0/annos/<string:anno_id>', methods=['GET'])
 def get_annotation(anno_id):
-    api_req = anno_api+anno_id
+    api_req = config['api_url']+anno_id
     response = requests.get(api_req)
     if response.status_code == 404:
         abort(404)
@@ -59,23 +65,19 @@ def create_annotation():
     if not request.json or not 'body' in request.json:
         abort(400)
     annotation = request.get_json()
-
-    #auth=('jants','brt1mrh2szn3')
-    #auth=('museeptionscuriandeteroc', '4378b87c00059e3443a07fc9f35227cbe286ff6d')
     auth=(config['key'], config['passphrase'])
-
-    api_req = anno_api
+    api_url = config['api_url']
     headers = {'content-type': 'application/json'}
-    response = requests.post(api_req,data=json.dumps(annotation),headers=headers,auth=auth)
+    response = requests.post(api_url,data=json.dumps(annotation),headers=headers,auth=auth)
     return jsonify({'response': response.json(), 'annotation': annotation}), 201
 
 
 # Update existing annotation
 @app.route('/annotations/api/v1.0/annos/<string:anno_id>', methods=['PUT'])
-def update_task(anno_id):
+def update_annotation(anno_id):
 #to do: fetch annotation
-    api_req = anno_api+anno_id
-    response = requests.get(api_req)
+    api_url = config['api_url']+anno_id
+    response = requests.get(api_url)
     db_rev = response.headers.get('Etag')
     if response.status_code == 404:
         abort(404)
@@ -88,36 +90,31 @@ def update_task(anno_id):
         abort(400)
 
 #update fields
-    # auth=('museeptionscuriandeteroc', '4378b87c00059e3443a07fc9f35227cbe286ff6d')
     auth=(config['key'], config['passphrase'])
     headers = {'content-type': 'application/json'}
     annotation = request.get_json()
+    # refer to cloudant docs update api
     annotation['_rev'] = db_rev.strip('"')
     
 #PUT DB
-    putResp = requests.put(api_req,data=json.dumps(annotation),headers=headers,auth=auth)
+    putResp = requests.put(api_url,data=json.dumps(annotation),headers=headers,auth=auth)
     return jsonify({'response': putResp.json(), 'annnotation': annotation})
 
 
 # Delete existing annotation
 @app.route('/annotations/api/v1.0/annos/<string:anno_id>', methods=['DELETE'])
 def delete_task(anno_id):
-    #auth = ('jants','brt1mrh2szn3')
-    #auth=('museeptionscuriandeteroc', '4378b87c00059e3443a07fc9f35227cbe286ff6d')
     auth=(config['key'], config['passphrase'])
-    api_req = anno_api+anno_id
+    api_req = config['api_url']+anno_id
     headers = {'content-type': 'application/json'}
     r = requests.get(api_req)
     rev = r.json()['_rev']
     api_rev=api_req+'?rev='+rev
     response = requests.delete(api_rev)
-    #response = requests.delete(api_req)
     if response.status_code == 404:
         abort(404)
-    # find annotation if not send error
-    # if found remove from DB
     return jsonify({'result': response.json() })
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host= '0.0.0.0')
+    app.run(debug=config['DEBUG'],host=config['HOST'])
